0813a061e7dc130975485c047cdfc0930268e1cc
Wouter Raateland <wouterraateland@MacBook-Pro-van-Wouter.local>
2018-12-13 12:33:56 +0100

    Implement 404 page

--- packages/ygh-website/src/pages/404.jsx
+++ packages/ygh-website/src/pages/404.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Layout from 'components/Layout'
+import { Wrapper } from 'components/ui'
+import Floor from 'components/hunt/objects/Floor'
+
+const FullHeightWrapper = styled(Wrapper)`
+  position: relative;
+  overflow: hidden;
+
+  height: calc(100vh - 22.5em);
+  border-radius: 260% 120% 190% 250% / 250% 210% 180% 190%;
+  border: 5vw solid transparent;
+  box-shadow: inset 0 0 25vw 5vw #000c;
+
+  display: flex;
+
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
+    <FullHeightWrapper>
+      <Floor />
+      <h1>Nothing Here</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    </FullHeightWrapper>
   </Layout>
 )
 
