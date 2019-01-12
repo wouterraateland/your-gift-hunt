fe1876bec607663703ae8e8cea4597fed3690538
Wouter Raateland <wouterraateland@gmail.com>
2019-01-12 16:33:46 +0100

    Create Desk chair object

--- packages/ygh-player/src/components/objects/InstructionNote.jsx
+++ packages/ygh-player/src/components/objects/InstructionNote.jsx
@@ -66,7 +66,7 @@ const Text = styled.span`
 `
 
 const InstructionNote = props => (
-  <PhysicalObject width="2em" height="2em">
+  <PhysicalObject width="2em" height="3em">
     <Note {...props} z={.1}>
       <Paper />
       <PaperClip />
