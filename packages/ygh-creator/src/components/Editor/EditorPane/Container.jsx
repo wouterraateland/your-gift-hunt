027639a4fde3df78f011bcaabc0cfae111f896ff
Wouter Raateland <wouterraateland@gmail.com>
2019-03-12 16:10:00 +0100

    Center view over nodes when creator is loaded

--- packages/ygh-creator/src/components/Editor/EditorPane/Container.jsx
+++ packages/ygh-creator/src/components/Editor/EditorPane/Container.jsx
@@ -1,5 +1,9 @@
-import React, { useRef } from "react"
+import { NODE_TYPES } from "data"
+import React, { useContext, useEffect, useRef } from "react"
 import styled from "styled-components"
+
+import GameContext from "contexts/Game"
+
 import usePanZoom from "hooks/usePanZoom" //"use-pan-and-zoom"
 
 import ZoomControls from "./ZoomControls"
@@ -26,6 +30,7 @@ const PanContainer = styled.div`
 `
 
 const EditorPane = ({ children }) => {
+  const { nodes, getNodePosition } = useContext(GameContext)
   const container = useRef(null)
   const {
     transform,
@@ -40,6 +45,22 @@ const EditorPane = ({ children }) => {
     minZoom: 0.1,
     maxZoom: 2
   })
+
+  useEffect(() => {
+    const ps = nodes
+      .filter(({ type }) => type === NODE_TYPES.STATE)
+      .map(({ id }) => getNodePosition(id))
+
+    if (container.current && ps.length) {
+      const meanX = ps.reduce((acc, p) => acc + p.left + 96, 0) / ps.length
+      const meanY = ps.reduce((acc, p) => acc + p.top + 48, 0) / ps.length
+
+      setPan({
+        x: container.current.offsetWidth / 2 - meanX,
+        y: container.current.offsetHeight / 2 - meanY
+      })
+    }
+  }, [])
 
   return (
     <StyledEditorPane
--- packages/ygh-creator/src/hooks/useGraphLayout.js
+++ packages/ygh-creator/src/hooks/useGraphLayout.js
@@ -192,6 +192,7 @@ const useGraphLayout = ({ nodes, edges }) => {
   )
 
   return {
+    nodePositions,
     getNodePosition
   }
 }
