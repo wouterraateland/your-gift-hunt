e1478ab75a2ce3459fddc30fcd530527ff02a7f0
Wouter Raateland <wouterraateland@gmail.com>
2019-04-14 14:32:28 +0200

    Update item screen visual

--- packages/ygh-player/src/your-gift-hunt/entityDetails/Screen.jsx
+++ packages/ygh-player/src/your-gift-hunt/entityDetails/Screen.jsx
@@ -1,30 +1,44 @@
-import React, { useRef } from 'react'
-import styled, { css } from 'styled-components'
+import React, { useRef } from "react"
+import styled, { css } from "styled-components"
 
 const Screen = styled.div`
   position: fixed;
-  left: 0; top: 0;
-  right: 0; bottom: 7em;
+  top: 0;
+  right: 0;
   z-index: 2;
 
   overflow-x: hidden;
   overflow-y: auto;
 
-  ${props => props.centerContent && css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  `}
+  @media (orientation: portrait) {
+    left: 0;
+    bottom: 5.5em;
+  }
+
+  @media (orientation: landscape) {
+    left: 5.5em;
+    bottom: 0;
+  }
+
+  ${props =>
+    props.centerContent &&
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+    `}
 
   background-color: #0004;
 
-  transition: opacity .2s ease-out;
+  transition: opacity 0.2s ease-out;
 
-  ${props => !props.isVisible && css`
-    pointer-events: none;
-    opacity: 0;
-  `}
+  ${props =>
+    !props.isVisible &&
+    css`
+      pointer-events: none;
+      opacity: 0;
+    `}
 `
 
 Screen.defaultProps = {
@@ -32,17 +46,13 @@ Screen.defaultProps = {
   isVisible: false
 }
 
-export default ({
-  onClick=(() => {}),
-  ...props
-}) => {
+export default ({ onClick = () => {}, ...props }) => {
   const screen = useRef(null)
 
   return (
     <Screen
       ref={screen}
-      onClick={event => event.target === screen.current
-        ? onClick() : null}
+      onClick={event => (event.target === screen.current ? onClick() : null)}
       {...props}
     />
   )
